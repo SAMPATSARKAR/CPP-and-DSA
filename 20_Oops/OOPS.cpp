4a3ebe63a62d6f9c169d23088272f18d1e896fdc
Sampat Sarkar <sampat7123@gmail.com>
2025-09-11 16:26:17 +0000

    Completed till inheritance

--- 20_Oops/OOPS.cpp
+++ 20_Oops/OOPS.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 class hero{
     //properties
     private:
-    public:
-    hero(){
-    cout<<"Sampat is great"<<endl;
-    };
     int health;
-
     char level;
+    public:
+    char *name= new char[100];
+    //constructor
+    hero(){
+        cout<<"Sampat is great"<<endl;
+        // name= new char[100];
+    };
+    //paramaterized constructor
+    hero(int health){
+        cout<<"this->"<<this<<endl;
+        this -> health = health;
+    }
+    //copy constructor
+    hero(hero& temp){
+        cout<<"copy constructor called"<<endl;
+        char* ch = new char[strlen(temp.name)+1];
+        strcpy(ch,temp.name);
+        this->name=ch;
+        this->health=temp.health;
+        this->level=temp.level;
+    }
     void print(){
-        cout<<level<<endl;
+        cout<<"[Name:-"<<this->name<<","<<endl;
+        cout<<"health:-"<<this->health<<","<<endl;
+        cout<<"level"<<this->level<<"]"<<endl;
     }
     //getter--fetch/read
     int getHealth(){
@@ -27,20 +47,42 @@ class hero{
     void setlevel(char ch){
         level = ch;
     }
+    void setname(char *n){
+        strcpy(this->name,n);
+    }
 
 };
+
 int main(){
+    hero h1;
+    h1.setHealth(70);
+    h1.setlevel('A');
+    char name[4] = "Sam";
+    h1.setname(name);
+    h1.print();
+
+    hero h2(h1);
+    h2.print();
+
+    h1.name[0]='A';
+    h1.print();
+    h2.print();
+    //constructor
+    // hero ramesh;---hero ramesh(10){parameterised constructor}
+    // cout<<"Address is->"<<&ramesh<<endl;
+    // ramesh.getHealth();
     
     //hero() method
 
     //obj statically
     // hero sampat;
+
     // obj dynamically
     // hero *sampat1 = new hero;
 
 
     //static allocation
-    //creation of Obj
+    //creation of Obj 
     // hero ramesh;
     // ramesh.health=70;
     // ramesh.level='A';
---
+++ 20_Oops/encapsulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class student{
+    private:
+    int age;
+    int height;
+
+    public:
+    
+    int getAge(){
+        return this->age;
+    }
+};
+
+int main(){
+    student first;
+
+    cout<<"Sab sahi ha"<<endl;
+
+    return 0;
+}
